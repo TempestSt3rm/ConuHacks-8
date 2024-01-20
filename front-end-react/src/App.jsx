a9fe556d2a97cc07a9a68906e6b6e1867c8c0413
Vincent He <hevincent23@gmail.com>
2024-01-20 16:00:49 -0500

    added a calendar, fixed the spacing of the links

--- front-end-react/src/App.jsx
+++ front-end-react/src/App.jsx
@@ -22,11 +22,8 @@ function App() {
 
   return (
     <>
-    <div>
       <Vincent></Vincent>
       {/* <h1>The Tire Change Shop</h1> */}
-    </div>
-      <div>
      
         {/* <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -34,7 +31,6 @@ function App() {
         {/* <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a> */}
-      </div>
       {/* <h1>Vite + React</h1> */}
       {/* <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
--- front-end-react/src/components/Vincent.jsx
+++ front-end-react/src/components/Vincent.jsx
@@ -3,12 +3,14 @@ import tireChangeImage from '../assets/images/tirechange.jpeg';
 export default function Vincent() {
     
     return (
-        <div> 
+        <> 
             <h1 className="tireChange">The Tire Change Shop</h1>
             <img src={tireChangeImage} className="tireChangeImage" alt="Tire Change Shop" />
             <a href="/schedule" className="schedule">Schedule</a>
             <a href="/revenue" className="revenue">Revenue</a>
-            <a href="Customer Numbers" clasName="customerNumbers">Customer Numbers</a>
-        </div>
+            <a href="/customerNumbers" clasName="customerNumbers">Customer Numbers</a>
+            <input type="date" name="Calendar" id="calendar" />
+        </>
     )
+
 }
