584d35f3bdf0d907c481a1bfa740be59de1e1e90
Mike Lin <mastermaiku3@gmail.com>
2024-01-20 14:43:14 -0500

    Add sorting functionality to API endpoint

--- Api/app.py
+++ Api/app.py
@@ -1,9 +1,22 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
+import csv
+from datetime import datetime
 
 app = Flask(__name__)
 
 CORS(app)
+
+@app.route('/')
+def index():
+    array = []
+    with open('datafile.csv', 'r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            array.append(row)
+        array = sorted(array)
+    return jsonify(array)
+
 
 @app.route('/api/data')
 def get_data():
