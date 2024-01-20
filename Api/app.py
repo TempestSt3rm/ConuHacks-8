c2ee76b0e044ddd5793916a1220690d86c878066
Mike Lin <mastermaiku3@gmail.com>
2024-01-20 15:23:19 -0500

    Add SQLAlchemy integration and database migration

--- Api/app.py
+++ Api/app.py
@@ -2,21 +2,42 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 import csv
 from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+db = SQLAlchemy(app)
 
 CORS(app)
 
-@app.route('/')
-def index():
+class Entry(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    timeOfRequest = db.Column(db.String(50), nullable=False)
+    timeOfAppointment = db.Column(db.String(50), nullable=False)
+    typeOfVehicle = db.Column(db.String(50), nullable=False)
+
+    def __repr__(self):
+        return '<Entry %r>' % self.id
+
+with app.app_context():
     array = []
     with open('datafile.csv', 'r') as file:
         reader = csv.reader(file)
         for row in reader:
             array.append(row)
         array = sorted(array)
-    return jsonify(array)
+        for element in array:
+            New_entry = Entry(timeOfRequest=element[0], timeOfAppointment=element[1], typeOfVehicle=element[2])
+            db.session.add(New_entry)
+    db.create_all()
+    db.session.commit()
 
+@app.route('/')
+def index():
+    array = []
+    for i in Entry.query.all():
+        array.append([i.id,i.timeOfRequest, i.timeOfAppointment, i.typeOfVehicle])
+    return jsonify(array)
 
 @app.route('/api/data')
 def get_data():
