8b38fdc4adb83b189d6f2a6a6f98e1134cc740f0
Jeff <jeffreywjlim@gmail.com>
2024-01-20 14:18:46 -0500

    backend

--- front-end-react/src/App.jsx
+++ front-end-react/src/App.jsx
@@ -12,6 +12,7 @@ function App() {
     axios.get('http://localhost:5000/api/data')
       .then(response => {
         setData(response.data.message);
+        
       })
       .catch(error => {
         console.error('Error fetching data:', error);
