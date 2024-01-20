6f10769856fe0c2d8b18f5f4028804cd951094c4
Blacklotus89898 <song.yang.chen89898@gmail.com>
2024-01-20 13:08:34 -0500

    added fe requesst

--- front-end-react/src/App.jsx
+++ front-end-react/src/App.jsx
@@ -1,10 +1,22 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import axios from 'axios';
 
 function App() {
+  const [data, setData] = useState('');
   const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/data')
+      .then(response => {
+        setData(response.data.message);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
 
   return (
     <>
