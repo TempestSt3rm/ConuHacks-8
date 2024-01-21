d5e67b4c0a3d73dafd4de90443f17dcfaf140298
Vincent He <hevincent23@gmail.com>
2024-01-21 10:17:43 -0500

    added dashboard stats

--- front-end-react/src/components/Dashboard.jsx
+++ front-end-react/src/components/Dashboard.jsx
@@ -1,14 +1,20 @@
 import Navbar from "./Navbar"
 
 function Dashboard() {
+
+    let totalRevenue = 1124350;
+    let totalRevenueTurnedAway = 1730650;
+    let percentageProfit = (totalRevenue / (totalRevenue + totalRevenueTurnedAway)) * 100; 
+    let roundedpercentageProfit = percentageProfit.toFixed(2);
     return (
         <>
         <Navbar></Navbar>
-        <h1>This is Dashboard</h1>
+        <h1 className="dataSummary">Data Summary</h1>
         <ul>
-        <li>Table</li>
-        <li>Receipt</li>
-        <li>Chart</li>
+        <li>Total Revenue $1124350</li>
+        <li>Total Revenue Turned Away $1730650</li>
+        <li>Total Customers Served 4791</li>
+        <li>Percentage Profit {roundedpercentageProfit}%</li>
         </ul>
         </>
     )
